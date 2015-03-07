2b625407ded967924a8d857a594cb23815abb78f
Elizabeth Pizzuti <elizabeth36@gmail.com>
2015-03-07 22:31:57 +0000

    Create anchored navigation

--- js/main.js
+++ js/main.js
@@ -1,172 +1,6 @@
-
-//This is a comment!
-
-console.log('Welcome message. Yay!!');
-
-// name of variable on the left - value on the right
-var welcomeMessage = 'Hello everybody. Welcome to Steer!';
-var numOfStudents = 13;
-
-console.log(numOfStudents);
-
-
-// What can JS even do
-// - change the html on our page
-// - change the css on our page
-// - add/remove html or blocks of content
-
-// Selecting something on the page
-// This is vanilla JS (not using a library)
-// document.getElementById('title').innerHTML = 'welcome';
-
-// using jQuery to make our life easier
-// $('#title').text(welcomeMessage);
-
-// jQuery selector
-// $('#about') - select something from html by id
-// $('.panel') - select something using the tagname
-// $('.alert')
-// $('section p') - select something using the tagname
-// $'input[type="submit"]') - anything that works in CSS 
-
-
-// select the header of the page. change the html to be an image.
-// $('header').html('<img src="http://placehold.it/800x100">');
-
-// change the css of the h2 and h3
-// $('h4, h3').css('color', '#D04171');
-
-// When somebody clicks on the header
-// $('header') select the element 
-// $('header').on(); on() is for listening to user events
-
-// can be helpful to read backwards - click, on, header...
-$('header').on('click', function(){
-	// inside of here is our list of instructions
-	console.log('Clicking on the header!!');
-
-	// change css
-	$('h1').css('border-color', '#D04171');
-
-	// change text
-	$('h1').text(' Big fan of Javascript ');
-
-});
-
-// Select the button, on click send message to console
-// Get the value from our form
-// $('.btn-attend').on('click', function(){
-
-	// //var name = 'sam';
-	
-	// var name = $('.input-name').val();
-
-	// console.log(name + ' is clicking on the button');
-
-	// // remove an element from the html
-	// $('form').remove();
-
-	// // add some new html to the page
-	// $('.signup').html('<div class="alert"> Thanks for attending ' + name + '</div>');
-
-	// // jQuery built in animations
-	// $('.alert').fadeIn(600);
-
-// });
-
-// Home Page
-// ---------
-
-// Select the view events button.
-// Check it's working using the console
-// look at how we can animate to contents
-
-
-$('.view-events').on('click', function(){
-	//list of instructions
-	console.log('clicking on the view events button');
-
-	// Use this keyword to relate to whatever just happened -which one did we just click on? this one.
-	// $(this).css('background-color', '#D04171');
-
-	// We can use parent(), children(), next(), to navigate around the html
-	// $(this).parent().css('background-color', '#53C8D5');
-
-	// move from the button we clock on to the paret  - 
-	// $(this).parent().next().fadeToggle();
-
-	$(this).parent().next().slideToggle();
-
-	// we can add and remove classes using jQuery - this allows us to keep our styles in our stylesheet
-	$(this).parent().toggleClass('selected');
-
-	// $('.item-body').fadeIn();
-});
-
-// use .hide() to hide elements on our page when the website loads
-$('.item-body').hide();
-
-
-// Lightbox JS - Data Attributes
-
-// click on one of the event thumbnails
-// lightbox to fade in
-// select the data from the html of the corrent event
-// fill in the information within the lightbox using the data
-
-// when lightbox is open, click on anything else
-// will cause the lightbox to fade out
-
-$('.event').on('click', function(){
-	console.log('clicking on event thumb');
-	// console.log( this );
-
-	//select data from the html
-	var eventName = $(this).data('name');
-	var imageName = $(this).data('image');
-	var user = $(this).data('user');
-	var attendees = $(this).data('attendees');
-	var date = $(this).data('date');
-
-	// change one attribute of an html element
-	$('.lightbox-inner img').attr('src', 'images/' + imageName + '.jpg');
-	$('.lightbox-content h2').text(eventName); 
-	$('.lightbox-content .creator').text(user);
-	$('.lightbox-content .date').text(date);
-	$('.lightbox-content .attendees').text(attendees);
-
-
-	console.log( eventName );
-	console.log( imageName );
-
-	//custom jQuery animation
-	$('.lightbox-inner').animate({
-		'top': '40%'
-		
-	})
-
-	$('#lightbox').fadeIn();
-
-
-});
-
-// click event for fading out lightbox
-$('#lightbox').on('click', function(){
-
-	// animate takes 3 options
-	// 1. css to animate
-	// 2. duration
-	// 3. function to run when animation finishes
-
-	$('.lightbox-inner').animate({
-		'top': '40%'
-	}, 1000, function(){
-		console.log('Where am I!!');
-		$('.lightbox-inner').css('top', '60%');
-	});
-		
-	$(this).fadeOut(1000);
-});
+// 1. Set display of logo to appear when nav appears
+// 2. Set display of logotype to disappear
+// 3. Have background image stay fixed in background and fade out (choose better image)
 
 
 
@@ -219,96 +53,155 @@ $('#lightbox').on('click', function(){
 // });
 
 
-// send a message to the console
-console.log('is it working?');
 
-// store information
+// Little Lunches JS 
 
-var name = 'sam';
+// Make sure our code only runs when the page ready - because our scripts are in the head of the html page
 
-console.log('my name is ' + name);
+$(document).ready( function(){
+	// All of our code goes within here - won't run until the page is ready
 
-var favoriteNumber = 7;
+	// trigger a CSS transition from JS - by removing a class
+	$('h1').removeClass('pre-animation');
 
-var instagramUrl = 'http://instagram.com/api/v1';
+// Screate smooth scrolling navigation
+$('nav a').on('click', function(){
+	console.log('clicking on navigation link?');
 
-// Create a set of instructions that happen when we click
+	var href = $(this).attr('href');
+	console.log('Clicked on the ' + href + ' link');
 
-$('.btn-attend').on('click', function(){
-	console.log('Is my button actually doing anything??');
+	// offset gives us an object so we can use the dot notation
+	var distanceFromTop = $(href).offset().top;
+	console.log(distanceFromTop);
 
-	$(this).css('background-color', 'D04171');
+	var options = {
+		scrollTop : distanceFromTop - 100
+	};
+
+	$('html, body').animate(options);
+	
+	// to scroll back to top, create a new button with scrollTop : 0
+
+	// console.log( $(href) );
+
+	// stop the default behaviour
+	// return false should be the last thing you write as anything below will be ignored
+	return false;
+});
+
+	// Create parallax scroll effect on iphone/ipad images
+	$(document).on('scroll', function(){
+		// this happens every single time you scroll
+		console.log('Scrolling');
+
+		var currentScrollPos = $(document).scrollTop();
+		// Math is a built in library - Takes number down to solid integer
+		var moveImagesBy = Math.floor(currentScrollPos / 14 );
+
+		// console.log('We have scrolled ' + currentScrollPos + 'px');
+
+		// create the parallax scroll effect, but make it slower by dividing into scroll position
+		$('.macbook').css('left', moveImagesBy  );
+		$('.iphone').css('right', moveImagesBy );
+
+	});
+
+	// Create a fixed navigation when we scroll into the intro section
+
+	$('#today').waypoint( function(direction){
+		//these instructions happen whenever we scroll to a section
+		console.log('We have reached a section! Direction: ' + direction);
+
+		if(direction === 'down'){
+			console.log('yes scrolling down');
+			// styled up the class 'fixed' by adding it to the html, once you're happy delete from the html and add dynamically with JS
+			$('nav').addClass('fixed');
+		} else {
+			console.log('nope scrolling up');
+			$('nav').removeClass('fixed');
+
+		}
+	});
+
+	$('section').waypoint(function(direction){
+		// Add active class to correct link within the navigation
+		var section = this.element.id;
+		console.log(section);
+
+		$('nav a').removeClass('active');
+		// add active class to the correct link
+		
+		$('nav a[href="#' + section + '"]').addClass('active');
+
+		// have an animation when we reach the about section
+		$('#' + section).find('.fadeInUp').removeClass('fadeInUp');
+
+	}, { offset : 150 });
+
+	// Get instagram images from the API
+
+	// token works to identify us to instagram, then can block us if we send too many requests.
+	// var token = '146e599e084f4a56aee2025d2ade9186';
+	// var userid = '1068444443';
+	// var getUserPhotosUrl = 'https://api.instagram.com/v1/users/' + userid + '/media/recent/?client_id=' + token + '&callback=?';
+
+	// // API request using $.getJSON -
+	// $.getJSON(getUserPhotosUrl, function(response){
+	// 	console.log(response);
+
+	// 	var photos = response.data;
+	// 	var testPhoto = photos[0].images.standard_resolution.url;
+	// 	// console.log( testPhoto );
+
+	// 	for(var i = 0; i < 4; i++){
+	// 		$('#instagram').children().eq(i).attr('src', photos[i].images.standard_resolution.url );
+
+	// 		// console.log(photos[i].images.standard_resolution.url);
+	// 	}
+
+	// });
+
+	// $('section').eq(2).css('background-color', 'green');
 
 
-var url = 'http://js-steer.herokuapp.com/events/';
+	$('.find-foundrs').on('click', function(){
 
-// get json from the url
-// create a set of instructions with function(){}
-// within the set of instructions use console.log to see the response
-// drill down into the response with the dot notation e.g. response.data.events
+		var lng = $(this).data('lng');
+		var lat = $(this).data('lat');
+		var type = 'coffee';
+	
 
-// var events - response.data.events;
+	// Search the Foursquare API
+	var id = '54HJX1UERJIB3Z4WV44TPM01RQ3AOJXKM1YAHPB3EF3XBSA4';
+	var secret = 'P0CTXFBGQHFATQYWHYNHGBY3RST1CTXCPPHGYRIXJW3BYQTU';
 
+	var foursquareUrl = 'https://api.foursquare.com/v2/venues/search?client_id=' + id + '&client_secret=' + secret + '&v=20130815&ll=' + lng + ',' + lat + '&query' + type;
 
-$.getJSON(url, function(response){
-// 	console.log(response);
+	// .getJSON with foursquareUrl
+	$.getJSON(foursquareUrl, function(response){
+		// console.log(response);
+		var venues = response.response.venues;
+		console.log(venues);
 
-// 	console.log(response.result);
+		for(var i = 0;i < 9; i++){
+			console.log( venues[i].name );
 
-// Events is an array of 3 events [Object,Object,Object]
-var events = response.data.events;
-// access one event using the specific number e.g events[0] to get the first
+			$('.foursquare-result').eq(i).text(venues[i].name);
 
-for(var counter = 0; counder <= 2; counter++){
-		// loop over from 0,1,2 and console.log each one
-		console.log ( events[counter].attendees );
+		}
 
-		// add each event name to the html
-		$('.new-events').append('<h2>'+ events[counter].name +'</h2>');
+		$('#foursquare').slideDown(500);
 
-}
+	});
+	
+	// stop default button behaviour
+	return false;
 
-}
+});
 
-// if ( response.result === 'success') {
+});
 
-// 	var events = response.data.events;
-
-// } else {
-
-// 	console.log('something went wrong');
-
-// }
-
-
-// });
-
-
-// });
-
-// Array - List of Information
-
-// var name = 'sam'; // string
-// var favoriteNumber = 7; // integer
-// var car = {
-// 	doors : 5,
-// 	colour: 'blue'
-
-// } // Object - car.doors
-
-// var shoppingList = ['oats', 'onions', 'eggs', 'coffee'];
-
-// console.log("Shopping list -", shoppingList[0]);
-
-// // Loop over information - lots of the same thing with a small amount of code
-
-// // start our counter, keep going while this equation is true
-// for(var counter = 0; counter <= 4; counter++){
-// 	console.log("The counter is currently at: " + counter);
-
-// 	console.log( shoppingList[counter] );
-
-// }
 
 
 
